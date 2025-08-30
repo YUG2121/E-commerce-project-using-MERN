959ef246a6d1304ae9f627a2d8a6fa4ad793895c
Yug Upadhyay <114340240+YUG2121@users.noreply.github.com>
2025-08-30 19:41:20 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 import { Route, Routes } from "react-router-dom";
-import Add from "./pages/Add";
-import List from "./pages/list";
-import Order from "./pages/Order";
-import Login from "./components/Login";
+import Add from "./pages/Add.jsx";
+import List from "./pages/list.jsx";
+import Order from "./pages/Order.jsx";
+import Login from "./components/Login.jsx";
 import { ToastContainer, toast } from "react-toastify";
 
 export const backendurl = import.meta.env.VITE_BACKEND_URL;
