79dc1f7c87c3e9ed2d8329e3fd1040fb6c901d22
Yug Upadhyay <114340240+YUG2121@users.noreply.github.com>
2025-08-30 22:09:08 +0530

    Add files via upload

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -5,5 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-   server:{port:5173}
+   server:{port:5173},
+   build: {
+  rollupOptions: {
+      external: ['react-router-dom'] // Only if necessary for specific build configurations
+        }
+
+      }
 });
